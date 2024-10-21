750cfccdcf7162a2022e234b17f02d51e2f341c3
KunalSalunkhe12 <salunkhekunal594@gmail.com>
2024-10-21 17:11:41 +0530

    seasonal planning half

---
+++ app/(tools)/seasonal-planning/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SeasonalPlanningForm from "@/components/seasonal-planning/input-form";
+
+const page = () => {
+  return (
+    <div className="p-5 md:p-10">
+      <SeasonalPlanningForm />
+    </div>
+  );
+};
+
+export default page;
