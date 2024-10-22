394a2b8c478fd44b7b09811fc2f61f473adc0f16
DawoodKhatri <dawoodkhatri18@gmail.com>
2024-10-22 09:30:58 +0530

    Removed Unused Pages

--- app/(tools)/cost-to-serve-analysis/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const page = () => {
-  return <div className="p-5 md:p-10"></div>;
-};
-
-export default page;
--- app/(tools)/distributed-inventory/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const DistributedInventoryToolPage = () => {
-  return (
-    <div>DistributedInventoryToolPage</div>
-  )
-}
-
-export default DistributedInventoryToolPage
